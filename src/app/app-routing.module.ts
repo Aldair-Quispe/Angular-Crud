370cb31ea80fe2d22b36bf70a2b0c284677a0c36
Aldair <aldaraboloco@gmail.com>
2022-12-12 19:27:36 -0400

    final bueno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { EditComponent } from "./components/edit/edit.component";
 import { ShowComponent } from './components/show/show.component';
 
 const routes: Routes = [
-    {path: ' ', component:ShowComponent},
+    {path: '', component:ShowComponent},
     {path: 'create', component:CreateComponent},
     {path: 'edit/:id', component:EditComponent},
 ];
